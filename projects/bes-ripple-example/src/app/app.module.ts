b1cc5e28cdda36aff07b8bca99ef236430e34a09
BeSaRa <besara@windowslive.com>
2022-04-14 08:17:48 +0300

    remove Browser Animation Module from the bes-ripple Module

--- projects/bes-ripple-example/src/app/app.module.ts
+++ projects/bes-ripple-example/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { BesRippleModule } from 'bes-ripple'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BesRippleModule],
-
+  imports: [BrowserModule, BrowserAnimationsModule, BesRippleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- projects/bes-ripple/src/lib/bes-ripple.module.ts
+++ projects/bes-ripple/src/lib/bes-ripple.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core'
 import { RippleDirective } from './ripple.directive'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [RippleDirective],
-  imports: [BrowserAnimationsModule],
-  exports: [BrowserAnimationsModule, RippleDirective],
+  imports: [],
+  exports: [RippleDirective],
 })
 export class BesRippleModule {}
